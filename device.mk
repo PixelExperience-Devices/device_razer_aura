2e25f98d0a42e28b5940d0fb76aa3965c696006e
jjpprrrr <zhongcy95@gmail.com>
2021-12-11 22:16:10 +0800

    aura: turn off compressed apex
    Google introduced compressed apex on android 12 [1], and the flag is
    enabled by default in updatable_apex.mk [2]. Aura cannot boot
    compressed apex with encryption, so disable it.
    
    ref:
    [1] https://source.android.com/devices/tech/ota/apex#compressed-apex
    [2] https://android.googlesource.com/platform/build/+/4a1240de8a2ee5795a30df123058677d3999145e
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- device.mk
+++ device.mk
@@ -12,6 +12,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+OVERRIDE_PRODUCT_COMPRESSED_APEX := false
+
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/razer/aura/aura-vendor.mk)
 
