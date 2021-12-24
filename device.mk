1c87dd0039860c03aaf9237bbd93bf2dc7b417cd
Alcatraz323 <alcatraz32323@gmail.com>
2021-12-24 11:39:49 +0800

    aura: remove deprecated library announce xml

--- device.mk
+++ device.mk
@@ -138,10 +138,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
-# ANT+
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml
-
 PRODUCT_PACKAGES += \
     AntHalService \
     com.dsi.ant.antradio_library
