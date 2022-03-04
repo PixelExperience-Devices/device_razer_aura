3cac76691d28bafad4f7136456bc4ea5c4db0d48
Etienne Ruffieux <eruffieux@google.com>
2022-03-04 13:34:57 +0000

    aura: Removed audio A2dp and Hearing Aid from device.mk
    Bug: 220306859
    Test: atest
    Tag: #feature
    Change-Id: I300369d36399ffb558b98bafc2478b872d09faba

--- device.mk
+++ device.mk
@@ -110,7 +110,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl:32 \
     android.hardware.audio.effect@4.0-impl:32 \
     android.hardware.audio.service \
-    audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
     libqcompostprocbundle \
