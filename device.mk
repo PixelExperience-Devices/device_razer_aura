bdaf058aaa3fb6f287167aca8e76572164467505
Alcatraz323 <alcatraz32323@gmail.com>
2022-04-12 11:09:26 +0800

    aura: downgrade to audio HAL 4.0
    * prebuilt audioHAL is 4.0
    * fix micphone blow on tws
    * fix weird microphone volume
    * fix weird volume curve

--- device.mk
+++ device.mk
@@ -108,8 +108,8 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@5.0-impl:32 \
-    android.hardware.audio.effect@5.0-impl:32 \
+    android.hardware.audio@4.0-impl:32 \
+    android.hardware.audio.effect@4.0-impl:32 \
     android.hardware.audio.service \
     audio.a2dp.default \
     audio.r_submix.default \
