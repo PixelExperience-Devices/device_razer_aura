00f2823d19222a81962eab09fb3fa61023738ea7
Alcatraz323 <alcatraz32323@gmail.com>
2023-11-22 11:54:32 +0800

    Revert "aura: RazerParts: disable resolution switch"
    This reverts commit 7e8069d2f5f178e53fe59d26ba34ec96473474c8.

--- RazerParts/src/com/razer/parts/DeviceSettingsFragment.java
+++ RazerParts/src/com/razer/parts/DeviceSettingsFragment.java
@@ -94,7 +94,7 @@ public class DeviceSettingsFragment extends PreferenceFragment implements Prefer
     }
 
     private void findPreferences() {
-        // mResolutionPref = findPreference(SCREEN_RESOLUTION);
+        mResolutionPref = findPreference(SCREEN_RESOLUTION);
         mRefreshRatePref = findPreference(SCREEN_REFRESH_RATE);
         mChromaPref = findPreference(CHROMA);
         mDolbyAtmosPref = findPreference(DOLBY_ATMOS);
@@ -102,7 +102,7 @@ public class DeviceSettingsFragment extends PreferenceFragment implements Prefer
     }
 
     private void bindListeners() {
-        // mResolutionPref.setOnPreferenceChangeListener(this);
+        mResolutionPref.setOnPreferenceChangeListener(this);
         mRefreshRatePref.setOnPreferenceChangeListener(this);
         mChromaPref.setOnPreferenceClickListener(this);
         mDolbyAtmosPref.setOnPreferenceClickListener(this);
@@ -110,7 +110,7 @@ public class DeviceSettingsFragment extends PreferenceFragment implements Prefer
     }
 
     private void updateSummary() {
-        // updateResolutionSummary();
+        updateResolutionSummary();
         updateRefreshRateSummary();
     }
 
